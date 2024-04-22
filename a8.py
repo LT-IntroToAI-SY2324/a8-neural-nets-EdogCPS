95fc632aa05d3aebf7ccc01dbaa0862a874cb37a
Ewan <ejmcphilliam@cps.edu>
2024-04-22 11:05:10 -0500

    new file:   __pycache__/neural.cpython-39.pyc 	new file:   __pycache__/utilities.cpython-39.pyc 	modified:   a8.py

--- a8.py
+++ a8.py
@@ -3,5 +3,14 @@ from neural import *
 print("<<<<<<<<<<<<<< XOR >>>>>>>>>>>>>>\n")
 
 
+xor_data = [
+    ([0,0], [0]),
+    ([1,0], [1]),
+    ([0,1], [1]),
+    ([0,0], [0])
+]
 
+xor_nn = NeuralNet(2, 2, 1)
+xor_nn.train(xor_data)
 
+print(xor_nn.test_with_expected(xor_data))
